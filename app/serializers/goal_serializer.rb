efa95a6ff6227e264eb321a63d8180ef1b6d7124
yassimortensen <yassimortensen@gmail.com>
2018-01-25 00:39:27 -0500

    working on seed file

--- app/serializers/goal_serializer.rb
+++ app/serializers/goal_serializer.rb
@@ -1,4 +1,10 @@
 class GoalSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :name, :description, :frequency, :binary, :amount, :unit, :logs
 
+  # def logs
+  #   {
+  #     logs: object.logs.sort_by {|log| log.date}
+  #   }
+  # end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,27 +3,15 @@ User.create(name: "Yassi", password: "blahblah", email: "yassimortensen@gmail.co
 
 Goal.create(user_id: 1, name: "Meditate", description: "Meditate every morning", frequency: 5, binary: TRUE)
   365.times do |count|
-    if count === 0
-      Log.create(goal_id: 1, date: Date.today, binary_input: FALSE)
-    else
-      Log.create(goal_id: 1, date: Date.today-count, binary_input: [TRUE, FALSE].shuffle[0])
-    end
+      Log.create(goal_id: 1, date: Date.today-365+count, binary_input: [TRUE, FALSE].shuffle[0])
   end
 
 Goal.create(user_id: 1, name: "Water Intake", description: "Drink 90oz of water daily", frequency: 7, binary: FALSE, amount: 90, unit: "oz")
   365.times do |count|
-    if count == 0
-      Log.create(goal_id: 2, date: Date.today, amount_input: 90)
-    else
-      Log.create(goal_id: 2, date: Date.today-count, amount_input: [40, 50, 60, 70, 80, 90, 100].shuffle[0])
-    end
+      Log.create(goal_id: 2, date: Date.today-365+count, amount_input: [40, 50, 60, 70, 80, 90, 100].shuffle[0])
   end
 
 Goal.create(user_id: 1, name: "Sleep", description: "Sleep at least 8 hours daily", frequency: 7, binary: FALSE, amount: 8, unit: "hours")
   365.times do |count|
-    if count == 0
-      Log.create(goal_id: 3, date: Date.today, amount_input: 8)
-    else
-      Log.create(goal_id: 3, date: Date.today-count, amount_input: (5..10).to_a.shuffle[0])
-    end
+      Log.create(goal_id: 3, date: Date.today-365+count, amount_input: (5..10).to_a.shuffle[0])
   end
